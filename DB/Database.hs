995bcc68aa8244bcbfecf7692352b9e2c059b504
caneroj1 <caneroj1@tcnj.edu>
2016-02-18 21:21:51 -0500

    initial db code

---
+++ DB/Database.hs
@@ -0,0 +1,43 @@
+module DB.Database
+(
+  connection,
+  close,
+  connect
+)
+where
+
+import           Data.Word
+import           Database.PostgreSQL.Simple
+import           System.Environment
+
+dbName :: IO String
+dbName = getEnv "DBNAME"
+
+dbHost :: IO String
+dbHost = getEnv "DBHOST"
+
+dbPass :: IO String
+dbPass = getEnv "DBPASS"
+
+dbUser :: IO String
+dbUser = getEnv "DBUSER"
+
+dbPort :: IO Word16
+dbPort = do
+  port <- getEnv "dbPort"
+  return $ fromIntegral (read port :: Int)
+
+connection :: IO ConnectInfo
+connection = do
+  host <- dbHost
+  port <- dbPort
+  user <- dbUser
+  pass <- dbPass
+  name <- dbName
+  return ConnectInfo {
+    connectHost     = host,
+    connectPort     = port,
+    connectUser     = user,
+    connectPassword = pass,
+    connectDatabase = name
+  }
